3c321dadba53e72205c59feec1cb33d3d988580d
Oded Goldglas <oded.godglas@fiverr.com>
2023-01-01 20:37:57 +0200

    Engine loop

--- src/engine.rs
+++ src/engine.rs
@@ -1,13 +1,31 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
+use wasm_bindgen::prelude::Closure;
+
 use crate::game::Game;
 use crate::log;
 use crate::model::GameEvent;
+use crate::web_utils::request_animation_frame;
 
 pub struct Engine {
     game: Rc<RefCell<Game>>,
     handled_events: Vec<GameEvent>,
+}
+
+impl Default for Engine {
+    fn default() -> Self {
+        Engine {
+            game: Rc::new(RefCell::new(Game::new())),
+            handled_events: vec![
+                GameEvent::Mousemove,
+                GameEvent::Mouseup,
+                GameEvent::Mouseleave,
+                GameEvent::Mouseenter,
+                GameEvent::Mousedown,
+            ],
+        }
+    }
 }
 
 impl Engine {
@@ -17,7 +35,38 @@ impl Engine {
         // Create game
 
         // Attach listeners
+        let engine = Engine::default();
 
-        log!("Engine launched");
+        log!("Engine launched {}", engine.handled_events.len());
+
+        engine.start_game_loop();
+    }
+
+    fn start_game_loop(&self) {
+        // This reference will point to the closure that will recursively called in each animation frame trigger.
+        // Thus this is a persistence RC which is used in all future iterations.
+        let main_loop = Rc::new(RefCell::new(None));
+
+        //This reference will hit the initial animation frame and be dropped by the end of this scope.
+        let initial_trigger = Rc::clone(&main_loop);
+
+        let game = Rc::clone(&self.game);
+        let mut iter = 0;
+        *initial_trigger.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+            let mut game = game.borrow_mut();
+            iter += 1;
+
+            game.run();
+
+            if iter > 150 {
+                log!("Game done");
+                let _ = main_loop.borrow_mut().take();
+                return;
+            }
+
+            request_animation_frame(main_loop.borrow().as_ref().unwrap());
+        }) as Box<dyn FnMut()>));
+
+        request_animation_frame(initial_trigger.borrow().as_ref().unwrap());
     }
 }
--- src/web_utils.rs
+++ src/web_utils.rs
@@ -1,10 +1,22 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 #[macro_export]
 macro_rules! log {
     ($($t:tt)*) => {
         web_sys::console::log_1(&format!($($t)*).into())
     }
+}
+
+/// "window" API Getters
+pub fn window() -> web_sys::Window {
+    web_sys::window().expect("Global `window` is expected to be present.")
+}
+
+pub fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("`requestAnimationFrame` is expected to be found upon `window` API.");
 }
 
 /// Binds Rust's `panic` into web console logger.
